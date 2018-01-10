7d51dfef0fa5b33b135ba40073415c6e54f18360
Mikus <jansabanskis@gmx.com>
2018-01-10 19:16:48 +0000

    Create confirm.php

---
+++ src/confirm.php
@@ -0,0 +1,9 @@
+<?php
+namespace SPATApp;
+
+class Config{
+    static $dbHost = "helios.csesalford.com:3306";
+    static $dbUsername = "stc669";
+    static $dbPassword = "ixz4x6IL7Y8uqQUZ";
+    static $dbName = "stc669_spat";
+}
